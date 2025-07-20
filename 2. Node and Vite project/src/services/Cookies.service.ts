f7606dbd92c3e0dadbc02db7747d2f18888af9b5
Alex <alessandro.piccione.75@gmail.com>
2025-07-20 08:25:33 +0200

    cleanup

--- 2. Node and Vite project/src/services/Cookies.service.ts
+++ 2. Node and Vite project/src/services/Cookies.service.ts
@@ -1,33 +1,31 @@
-import { debug } from "@/utils"
+import { debug } from '@/utils'
 
-type cookie = "AuthToken" | "Username"
+type cookie = 'AuthToken' | 'Username'
 
 export default class CookieService {
-  static readCookie = (name:cookie, trigger:string) => {
-    debug(`readCookie ${name} (for ${trigger}): ${document.cookie}`)
+  static readCookie = (name: cookie, caller: string) => {
+    debug(`readCookie ${name} (for ${caller}): ${document.cookie}`)
     // TODO read all values of cookie
     const cookies = document.cookie.split('; ')
-    const cookie = cookies.find( c => c.startsWith(`${name}=`))
+    const cookie = cookies.find((c) => c.startsWith(`${name}=`))
     if (cookie) {
       debug(`cookie ${name}: ${cookie}`)
-      const value = cookie.split("=")[1]
+      const value = cookie.split('=')[1]
       return value
-    }
-    else {
+    } else {
       return null
     }
   }
   //    document.cookie.split('; ').find((c) => c.startsWith(`${name}=`))
   //      ?.split('=')[1] ?? null
 
-
   // can't use HttpOnly because it is not clear how the hell it works.
-  static setCookie = (name:cookie, value:string, expiresAt:Date) => {
+  static setCookie = (name: cookie, value: string, expiresAt: Date) => {
     document.cookie = `${name}=${value}; expires=${expiresAt.toUTCString()}; path=/; secure; SameSite=Strict;`
   }
 
-  static removeCookie = (name:cookie) => {
-    debug("remove cookie")
+  static removeCookie = (name: cookie) => {
+    debug('remove cookie')
     document.cookie = `${name}=; expires=${new Date().toUTCString()}; path=/; secure; SameSite=Strict;`
   }
 }
